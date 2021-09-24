f14dd57584b7dbf3fe2413b9a791bbfd85ae25bb
joshualectric <joshuainrecovery7@gmail.com>
2021-09-23 19:27:23 -0500

    created a page title component and styled it

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SignInForm from './signinForm'
+import PageTitle from '../pageTitle';
 
 import { reduxForm, Field } from 'redux-form';
 
@@ -11,6 +12,7 @@ class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
+                <PageTitle className='sign-in__page-title' title='Login' />
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
