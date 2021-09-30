1a500de94913716356dbe3be3898313327b0c1b9
joshualectric <joshuainrecovery7@gmail.com>
2021-09-30 17:54:44 -0500

    developed sign up form and modified formbutton to have short gray case

---
+++ src/components/auth/signupForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+import Details from '../details';
+
+import history from '../../history';
+
+import { FormInput, FormButton } from '../formFields';
+
+class SignUpForm extends Component {
+    render() {
+        const { className, handleSubmit} = this.props;
+        const links = [
+            {
+                _id: 0,
+                title: 'Not registered? Create account here',
+                onClick: () => hostory.push('/signup')
+            },
+            {
+                _id: 1,
+                title: 'Forgot account email?',
+                onClick: () => console.log('forgot email')
+            },
+            {
+                _id: 2,
+                title: 'Forgot password?',
+                onClick: () => console.log('forgot password')
+            }
+        ]
+        return (
+            <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
+                <Field className='sign-up-form__name' 
+                type='name' 
+                title='name' 
+                name='Name' 
+                placeholder='Name' 
+                component={FormInput} />
+                <Field className='sign-up-form__email' 
+                type='email' 
+                title='Email' 
+                name='email' 
+                placeholder='Email' 
+                component={FormInput} />
+                <Field className='sign-up-form__password' 
+                type='password' 
+                title='Password' 
+                name='password' 
+                placeholder='Password' 
+                component={FormInput} />
+                <Field className='sign-up-form__confirm-password' 
+                type='password' 
+                title='Confirm Password' 
+                name='confirm' 
+                placeholder='Confirm Password' 
+                component={FormInput} />
+                <div className='sign-up-form__line'></div>
+                <Field className='sign-up-form__login' 
+                onClick={() => console.log('tryna submit')}
+                type='submit' 
+                title='Login' 
+                name='login' 
+                component={FormButton} />
+                <Field className='sign-up-form__back'
+                onClick={() => console.log('tryna go back')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true} //Add this line of code
+                component={FormButton}/>
+                <Details className='sign-up-form__details' title='QuickLinks' links={links}/>
+            </form>
+        )
+    }
+}
+
+SignUpForm = reduxForm({
+    form: 'SignUpForm'
+})(SignUpForm);
+
+export default SignUpForm;
