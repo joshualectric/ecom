afc510648100af0a963433009c55ec481c4e916f
joshualectric <joshuainrecovery7@gmail.com>
2021-10-17 15:14:53 -0500

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,93 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                total: 19.40,
+                orderNumber: 'A004847493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40
+                total: 9.20,
+                orderNumber: 'B334847493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Joshua Steinberg',
+                    shippingAddress: '19 elm Street'
+                }
+            },
+            {
+                _id: 2,
+                total: 3.40,
+                orderNumber: 'C004847493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Yardley Huber',
+                    shippingAddress: '666 love street'
+                }
+            },
+            {
+                _id: 3,
+                total: 6.67,
+                orderNumber: 'ACG047493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Linda Lerner',
+                    shippingAddress: '1919 yourmom street'
+                }
+            },
+            {
+                _id: 4,
+                total: 23.40,
+                orderNumber: 'H236847493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Catherine Joyce',
+                    shippingAddress: '99 Wicked Stepmom drive'
+                }
+            },
+            {
+                _id: 5,
+                total: 19.40,
+                orderNumber: 'A004847493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 6,
+                total: 19.40,
+                orderNumber: 'A004847493',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 7,
+                total: 19.40,
+                orderNumber: 'BDFJE4567',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Andrew Hudgens',
+                    shippingAddress: '348 West State Street'
+                }
             }
+            
         ]
     })
 }
