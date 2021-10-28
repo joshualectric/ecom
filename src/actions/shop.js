34286f11abf2afbf52e46bd30f0bb33b6b0b4a4e
joshualectric <joshuainrecovery7@gmail.com>
2021-10-28 13:48:52 -0500

    fetch products action

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,11 +1,48 @@
 import {
    // SET_SHOP_CATEGORIES
-   SET_NAVBAR_LINKS
+   SET_NAVBAR_LINKS,
+   SET_SHOP_PRODUCTS
 } from './types';
 
 export function fetchShopCategories() {
     return ({
         type: SET_NAVBAR_LINKS,
+        payload: [
+            {
+                _id: 0,
+                title: 'All'
+            },
+            {
+                _id: 1,
+                title: 'JavaScript'
+            },
+            {
+                _id: 2,
+                title: 'UI/UX'
+            },
+            {
+                _id: 3,
+                title: 'Linux'
+            },
+            {
+                _id: 4,
+                title: 'Python'
+            },
+            {
+                _id: 5,
+                title: 'UML'
+            },
+            {
+                _id: 6,
+                title: 'Ruby'
+            },
+        ]
+    })
+}
+
+export function fetchShopProducts() {
+    return ({
+        type: SET_SHOP_PRODUCTS,
         payload: [
             {
                 _id: 0,
